a0866c586d1c60e18ac7277f8e8062ce681e5dfa
William Wu <dev.wwu@gmail.com>
2026-04-17 23:13:51 -0700

    Configure import linting

--- thumbtax-webapp/eslint.config.js
+++ thumbtax-webapp/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -18,6 +20,24 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      "no-relative-import-paths": noRelativeImportPaths,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "no-relative-import-paths/no-relative-import-paths": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react"],
+            ["^@?\\w"],
+            ["^#src", "^.+\\.css"],
+            ["^.*\\u0000$"],
+          ],
+        },
+      ],
     },
   },
 ]);
--- thumbtax-webapp/src/App.tsx
+++ thumbtax-webapp/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import "./App.css";
+
+import "#src/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
