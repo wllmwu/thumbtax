b9b2eaa57db5e3052d4ac79753f85c5b0829904e
William Wu <dev.wwu@gmail.com>
2026-05-14 20:06:18 -0700

    Install React Router and React Aria

--- thumbtax-webapp/vite.config.ts
+++ thumbtax-webapp/vite.config.ts
@@ -1,11 +1,20 @@
 /// <reference types="vitest/config" />
+import optimizeLocales from "@react-aria/optimize-locales-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...optimizeLocales.vite({
+        locales: ["en-US"],
+      }),
+      enforce: "pre",
+    },
+  ],
   resolve: {
     alias: {
       "#src": path.resolve(__dirname, "src"),
