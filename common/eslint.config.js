f2e0d669e0fdd109ba4784d92302be6449bf090c
William Wu <dev.wwu@gmail.com>
2026-06-21 15:12:39 -0700

    Init common package

---
+++ common/eslint.config.js
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+    plugins: {
+      "no-relative-import-paths": noRelativeImportPaths,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "no-relative-import-paths/no-relative-import-paths": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^@?\\w"], ["^#src", "^.+\\.css"], ["^.*\\u0000$"]],
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+]);
