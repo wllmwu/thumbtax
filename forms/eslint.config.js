fcc8386623c30804d35dd19efa804bb40191a86b
William Wu <dev.wwu@gmail.com>
2026-06-21 19:37:41 -0700

    Add forms workspace

---
+++ forms/eslint.config.js
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^@?\\w"], ["^.*\\u0000$"]],
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+]);
