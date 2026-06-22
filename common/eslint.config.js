991aeeca30e0fffc09b488098497eb2d702c963b
William Wu <dev.wwu@gmail.com>
2026-06-21 18:06:24 -0700

    Don't use #src alias in common because it breaks vite

--- common/eslint.config.js
+++ common/eslint.config.js
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
-import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
@@ -17,15 +16,13 @@ export default defineConfig([
       },
     },
     plugins: {
-      "no-relative-import-paths": noRelativeImportPaths,
       "simple-import-sort": simpleImportSort,
     },
     rules: {
-      "no-relative-import-paths/no-relative-import-paths": "error",
       "simple-import-sort/imports": [
         "error",
         {
-          groups: [["^@?\\w"], ["^#src", "^.+\\.css"], ["^.*\\u0000$"]],
+          groups: [["^@?\\w"], ["^.*\\u0000$"]],
         },
       ],
     },
