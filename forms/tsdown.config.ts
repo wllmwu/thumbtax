e7d0086b5487d4a5b78350766b95c14c980fc8d6
William Wu <dev.wwu@gmail.com>
2026-06-30 22:41:29 -0700

    Add deps.onlyBundle to tsdown config

--- forms/tsdown.config.ts
+++ forms/tsdown.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig([
   {
+    deps: {
+      onlyBundle: ["@markdoc/markdoc"],
+    },
     entry: ["./src/schema.ts"],
     outDir: "./schema-dist",
   },
   {
+    deps: {
+      onlyBundle: ["@markdoc/markdoc", "prettier"],
+    },
     entry: ["./src/buildForms.ts"],
     outDir: "./build-dist",
   },
