e94505a58a3b4a48a79725f8952961f010c3cd01
William Wu <dev.wwu@gmail.com>
2026-07-01 22:26:13 -0700

    Don't ignore forms/src/generated

--- forms/eslint.config.js
+++ forms/eslint.config.js
@@ -5,7 +5,7 @@ import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["build-dist", "dist", "schema-dist", "src/generated"]),
+  globalIgnores(["build-dist", "dist", "schema-dist"]),
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [js.configs.recommended, tseslint.configs.recommended],
