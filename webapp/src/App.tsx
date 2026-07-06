368ecaa4af1529dc54d043610ff0deffed3ef74d
William Wu <dev.wwu@gmail.com>
2026-07-05 22:23:53 -0700

    Remove specification client

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,18 +1,12 @@
-import React from "react";
-
+import { specifications } from "@thumbtax/forms";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { usePersistence } from "#src/persistence/usePersistence";
-import { SpecificationClient } from "#src/specifications/specificationClient";
 import { Layout } from "#src/ui/navigation/Layout";
 import { AboutPage } from "#src/ui/pages/AboutPage";
 import { MainPage } from "#src/ui/pages/MainPage";
 
 export function App() {
-  const specifications = React.useMemo(
-    () => new SpecificationClient().getAllForms(),
-    [],
-  );
   usePersistence(specifications);
 
   return (
