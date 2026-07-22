39937f3aa61e814126d71805cee8bfa5526eb1fc
William Wu <dev.wwu@gmail.com>
2026-07-21 23:02:04 -0700

    Lucide and React Aria class styles

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "#src/App.tsx";
 import "#src/index.css";
+import "#src/ui/library-styles/lucide.css";
+import "#src/ui/library-styles/react-aria.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
