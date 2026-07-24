16991b6df4e0e9d04e92a94afebd4ec4bc2688b2
William Wu <dev.wwu@gmail.com>
2026-07-23 22:43:16 -0700

    NavigationMenu styles

--- webapp/src/ui/navigation/NavigationMenu.tsx
+++ webapp/src/ui/navigation/NavigationMenu.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router";
 
+import styles from "#src/ui/navigation/NavigationMenu.module.css";
+
 export function NavigationMenu() {
   return (
-    <nav>
-      <ol>
+    <nav className={styles.navigationMenu}>
+      <span className={styles.wordmark}>📌 Thumbtax</span>
+      <ul>
         <li>
           <Link to="/">Main</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
         </li>
-      </ol>
+      </ul>
     </nav>
   );
 }
