df181e569ac4466a6f7cf92ca7c7a5f3c65f69e8
William Wu <dev.wwu@gmail.com>
2026-07-26 14:08:19 -0700

    Move Layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,8 +2,8 @@ import { specifications } from "@thumbtax/forms";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { usePersistence } from "#src/persistence/usePersistence";
-import { Layout } from "#src/ui/navigation/Layout";
 import { AboutPage } from "#src/ui/pages/AboutPage";
+import { Layout } from "#src/ui/pages/Layout";
 import { MainPage } from "#src/ui/pages/MainPage";
 
 export function App() {
