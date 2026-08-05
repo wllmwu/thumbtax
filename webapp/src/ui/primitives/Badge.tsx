0eb3786beb7bc944552ef256cc959146d0880e84
William Wu <dev.wwu@gmail.com>
2026-08-04 22:26:19 -0700

    Basic Badge styles

--- webapp/src/ui/primitives/Badge.tsx
+++ webapp/src/ui/primitives/Badge.tsx
@@ -1,7 +1,9 @@
+import styles from "#src/ui/primitives/Badge.module.css";
+
 type Props = {
   children: React.ReactNode;
 };
 
 export function Badge({ children }: Props) {
-  return <span>{children}</span>;
+  return <span className={styles.badge}>{children}</span>;
 }
