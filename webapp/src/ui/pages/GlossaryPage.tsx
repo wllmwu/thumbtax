ac53b2b7ecf76dc71cd0158063b1efa04035bb7b
William Wu <dev.wwu@gmail.com>
2026-08-09 11:21:54 -0700

    Render definitions in GlossaryPage

--- webapp/src/ui/pages/GlossaryPage.tsx
+++ webapp/src/ui/pages/GlossaryPage.tsx
@@ -1,3 +1,28 @@
-export function GlossaryPage() {
-  return <div>Glossary</div>;
+import React from "react";
+
+import { renderers } from "@markdoc/markdoc";
+import { glossary } from "@thumbtax/glossary";
+
+function GlossaryDefinition({
+  children,
+  term,
+}: {
+  children: React.ReactNode;
+  term: string;
+}): React.ReactNode {
+  return (
+    <div>
+      {term}
+      <br />
+      {children}
+    </div>
+  );
+}
+
+export function GlossaryPage(): React.ReactNode {
+  return React.useMemo(() => {
+    return renderers.react(glossary, React, {
+      components: { GlossaryDefinition },
+    });
+  }, []);
 }
