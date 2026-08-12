bbaa7b66d9dfd3a7a7a2874cbb5d3185211069e7
William Wu <dev.wwu@gmail.com>
2026-08-11 19:10:56 -0700

    Glossary styles

--- webapp/src/ui/pages/GlossaryPage.tsx
+++ webapp/src/ui/pages/GlossaryPage.tsx
@@ -5,21 +5,19 @@ import { glossary } from "@thumbtax/forms";
 import { ProseContent } from "#src/ui/content/ProseContent";
 
 export function GlossaryPage(): React.ReactNode {
-  const entries = React.useMemo(() => {
-    return Object.entries(glossary).map(([term, entry]) => (
-      <div key={term}>
-        <dt id={term}>{entry.name}</dt>
-        <dd>
-          <ProseContent nodes={entry.definition} />
-        </dd>
-      </div>
-    ));
-  }, []);
-
   return (
     <div>
       <h1>Glossary</h1>
-      <dl>{entries}</dl>
+      <dl>
+        {Object.entries(glossary).map(([term, entry]) => (
+          <div key={term}>
+            <dt id={term}>{entry.name}</dt>
+            <dd>
+              <ProseContent nodes={entry.definition} />
+            </dd>
+          </div>
+        ))}
+      </dl>
     </div>
   );
 }
