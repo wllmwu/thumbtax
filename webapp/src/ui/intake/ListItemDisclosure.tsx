002aa2e56135d5b472eff101e70fe4256f2503f6
William Wu <dev.wwu@gmail.com>
2026-09-06 16:53:45 -0700

    Confirm before deleting

--- webapp/src/ui/intake/ListItemDisclosure.tsx
+++ webapp/src/ui/intake/ListItemDisclosure.tsx
@@ -1,14 +1,23 @@
+import React from "react";
+
 import { ChevronDownIcon, ChevronRightIcon, Trash2Icon } from "lucide-react";
-import { Disclosure, DisclosurePanel } from "react-aria-components";
+import {
+  Dialog,
+  Disclosure,
+  DisclosurePanel,
+  Heading,
+  Modal,
+} from "react-aria-components";
 import { Controller, Watch } from "react-hook-form";
 
+import { AriaButton } from "#src/ui/primitives/AriaButton";
+import { DialogFooter } from "#src/ui/primitives/DialogFooter";
 import { IconButton } from "#src/ui/primitives/IconButton";
 import { MoveButton } from "#src/ui/primitives/MoveButton";
 import { racn } from "#src/ui/utils/racn";
 import styles from "#src/ui/intake/ListItemDisclosure.module.css";
 
 import type { IntakeWizardState } from "#src/ui/intake/types/intakeWizardState";
-import type React from "react";
 import type { Control } from "react-hook-form";
 
 type Props = {
@@ -42,48 +51,78 @@ export function ListItemDisclosure({
   onMoveBackward,
   onMoveForward,
 }: Props): React.ReactNode {
-  // TODO: confirm before remove
+  const [showDeleteConfirmDialog, setShowDeleteConfirmDialog] =
+    React.useState(false);
+
   return (
-    <Controller
-      control={control}
-      name={expandedFieldName}
-      render={({ field: expandedField }) => (
-        <Disclosure
-          className={racn(styles.disclosure, className)}
-          isExpanded={expandedField.value}
-        >
-          <div className={styles.header}>
-            <IconButton
-              icon={expandedField.value ? ChevronDownIcon : ChevronRightIcon}
-              label={expandedField.value ? "Collapse" : "Expand"}
-              onPress={() => expandedField.onChange(!expandedField.value)}
-            />
-            <Watch
-              control={control}
-              name={labelFieldName}
-              render={(label) => (
-                <span className={styles.title}>{label || "Untitled"}</span>
-              )}
-            />
-            <MoveButton
-              axis="block"
-              direction="backward"
-              isDisabled={!canMoveBackward}
-              onPress={onMoveBackward}
-            />
-            <MoveButton
-              axis="block"
-              direction="forward"
-              isDisabled={!canMoveForward}
-              onPress={onMoveForward}
-            />
-            <IconButton icon={Trash2Icon} label="Delete" onPress={onDelete} />
-          </div>
-          <DisclosurePanel className={racn(styles.disclosurePanel)}>
-            {children}
-          </DisclosurePanel>
-        </Disclosure>
-      )}
-    />
+    <>
+      <Controller
+        control={control}
+        name={expandedFieldName}
+        render={({ field: expandedField }) => (
+          <Disclosure
+            className={racn(styles.disclosure, className)}
+            isExpanded={expandedField.value}
+          >
+            <div className={styles.header}>
+              <IconButton
+                icon={expandedField.value ? ChevronDownIcon : ChevronRightIcon}
+                label={expandedField.value ? "Collapse" : "Expand"}
+                onPress={() => expandedField.onChange(!expandedField.value)}
+              />
+              <Watch
+                control={control}
+                name={labelFieldName}
+                render={(label) => (
+                  <span className={styles.title}>{label || "Untitled"}</span>
+                )}
+              />
+              <MoveButton
+                axis="block"
+                direction="backward"
+                isDisabled={!canMoveBackward}
+                onPress={onMoveBackward}
+              />
+              <MoveButton
+                axis="block"
+                direction="forward"
+                isDisabled={!canMoveForward}
+                onPress={onMoveForward}
+              />
+              <IconButton
+                icon={Trash2Icon}
+                label="Delete"
+                onPress={() => setShowDeleteConfirmDialog(true)}
+              />
+            </div>
+            <DisclosurePanel className={racn(styles.disclosurePanel)}>
+              {children}
+            </DisclosurePanel>
+          </Disclosure>
+        )}
+      />
+      <Modal
+        isDismissable
+        isOpen={showDeleteConfirmDialog}
+        onOpenChange={setShowDeleteConfirmDialog}
+      >
+        <Dialog>
+          <Watch
+            control={control}
+            name={labelFieldName}
+            render={(label) => <Heading>Delete {label || "Untitled"}?</Heading>}
+          />
+          <p>This cannot be undone.</p>
+          <DialogFooter>
+            <AriaButton onPress={() => setShowDeleteConfirmDialog(false)}>
+              Cancel
+            </AriaButton>
+            <AriaButton onPress={onDelete} variant="destructive">
+              Delete
+            </AriaButton>
+          </DialogFooter>
+        </Dialog>
+      </Modal>
+    </>
   );
 }
